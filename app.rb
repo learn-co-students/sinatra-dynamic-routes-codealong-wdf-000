a529fab0b0f1675c8aef2dddb9bb4e1794601c35
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-08-23 10:47:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num = num1 * num2
-     @num
+    @num = params[:num1].to_i * params[:num2].to_i
+     "#{@num}"
   end
 
 
--- spec/sinatradynamic_routes_codealong_spec.rb
+++ spec/sinatradynamic_routes_codealong_spec.rb
@@ -13,17 +13,17 @@ describe 'APP' do
     end
   end
 
-    # describe 'GET /multiply/:num1/:num2' do
-    # it 'sends a 200 status code' do
-    #   get '/multiply/5/5'
-    #   expect(last_response.status).to eq(200)
-    # end
-    #
-    # it 'displays product of route params' do
-    #   get '/multiply/5/5'
-    #   expect(last_response.body).to include("25")
-    # end
-  # end
+    describe 'GET /multiply/:num1/:num2' do
+    it 'sends a 200 status code' do
+      get '/multiply/5/5'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'displays product of route params' do
+      get '/multiply/5/5'
+      expect(last_response.body).to include("25")
+    end
+  end
 
 
 end

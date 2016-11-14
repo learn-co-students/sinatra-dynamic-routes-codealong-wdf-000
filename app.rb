7fed480fd1428092d0e93d9a4eb7b66d05602301
Ekaterina Petushova <epetushova@gmail.com>
2016-11-14 11:52:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -18,20 +18,22 @@ class App < Sinatra::Base
   	"Goodbye #{@user_name}!"
   end
 
+  # get "/multiply/:num1/:num2" do
+  # 	num_1 = params[:num1].to_i
+  # 	num_2 = params[:num2].to_i
+  # 	@product = num_1 * num_2
+  # 	"#{@product}"
+  # end
+
+  # or:
   get "/multiply/:num1/:num2" do
-  	num_1 = params[:num1].to_i
-  	num_2 = params[:num2].to_i
-  	@product = num_1 * num_2
+  @num_1 = params[:num1].to_i
+  	@num_2 = params[:num2].to_i
+  	@product = @num_1 * @num_2
   	"#{@product}"
   end
-
-  # get "/multiply/:num1/:num2" do
-  # @num_1 = params[:num1].to_i
-  #	@num_2 = params[:num2].to_i
-  #	@product = @num_1 * @num_2
-  #	"#{@product}"
-  #end
-
+  
+  # or:
   # get "/multiply/:num1/:num2" do
   # @product = params[:num1].to_i * params[:num2].to_i
   #	"#{@product}"
